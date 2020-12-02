99943ed92b394c7e9a5e4e736d887b68db6ad552
storyinvisible <storyinvisible@gmail.com>
2020-12-02 14:50:22 +0800

    return c

--- controllers/util/XOR.controllers.util.js
+++ controllers/util/XOR.controllers.util.js
@@ -17,14 +17,14 @@ function checkpwd (received, actual){
     for (i=0; i< received_hash.length;i++){
         c+=(parseInt(received_hash[i],16) ^ parseInt(actual_hash[i],16)).toString(16)
     }
-    new_int= []
-    for(i=0; i <recv_int.length; i++){
-        new_int.push(recv_int[i]^ actual_int[i])
-    }
+    // new_int= []
+    // for(i=0; i <recv_int.length; i++){
+    //     new_int.push(recv_int[i]^ actual_int[i])
+    // }
      
-    xor_results= createHexString(new_int)
+    // xor_results= createHexString(new_int)
     console.log(c)
-    console.log(xor_results)
+    return c
 }
 
 checkpwd("hello","byte");
