b1328733557eafed889a1a12ee9e640015fb2616
AnAsianGangster <AnAsianGangster@users.noreply.github.com>
2020-12-03 14:54:45 +0800

    quick fix

--- controllers/util/checkPwd.controllers.util.js
+++ controllers/util/checkPwd.controllers.util.js
@@ -4,11 +4,11 @@ exports.checkPwd = (received, actual) => {
     const receivedHash = md5(received);
     const actualHash = md5(actual);
 
-    let c = '';
+    let c = [];
 
     for (let i = 0; i < receivedHash.length; i++) {
-        c += (parseInt(receivedHash[i], 16) ^ parseInt(actualHash[i], 16)).toString(16);
+        c.push((parseInt(receivedHash[i], 16) ^ parseInt(actualHash[i], 16)).toString(16));
     }
 
-    return c;
+    return c.join('');
 };
